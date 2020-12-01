151788e27f0b751ad268e6a94d89894b27d5a516
Alejandro <ale_rusinoff13@hotmail.com>
2020-11-30 21:03:05 -0300

    se añadió el ejercicio1

---
+++ Ejercicio 1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import math 
+a=9
+b=7
+c=10
+d=math.sqrt(a)
+print(d)
+
+print("a:")
+print(a)
+print("b:")
+print(b)
+print("c:")
+print(c)
+resultado=a+b+c
+print("resultado")
+
+
+
